ef98573323bbc3c4ca57a621544f67131403ed9b
Lakshya Thakur <lapstjup@gmail.com>
2025-12-03 19:37:10 +0530

    remove motion warning

--- src/components/shared/Card.tsx
+++ src/components/shared/Card.tsx
@@ -7,15 +7,12 @@ interface CardProps {
   href: string;
 }
 
-const MotionLink = motion(Link);
+const MotionLink = motion.create(Link);
 
 export const Card = ({ children, href }: CardProps) => {
   return (
-
     <MotionLink initial={{ opacity: 0.5, filter: "blur(4px)" }}
       animate={{ opacity: 1, filter: "blur(0px)" }}
       transition={{ duration: 0.5 }} to={href} className="bg-zinc-900 h-fit text-white overflow-hidden rounded-xl cursor-pointer border border-zinc-800 hover:border-zinc-500 transition-colors duration-300 focus:outline-0 focus-visible:border-zinc-500">      {children}</MotionLink>
-
-
   );
 };
